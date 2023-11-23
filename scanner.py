b1d1ef03c2e0c0b1d1ec5ceb476cf7f1b7cd4e5b
Marie Bisbo <mariebisbo@Maries-MacBook-Pro.local>
2023-11-23 11:11:05 +0000

    Complete scanner

--- scanner.py
+++ scanner.py
@@ -1,5 +1,5 @@
 from lox_token import Token
-from token_type import TokenType
+from token_type import TokenType, keywords
 
 class Scanner:
     source: str
@@ -20,7 +20,7 @@ class Scanner:
         self.tokens.append(Token(TokenType.EOF, "", None, self.line))
 
         for token in self.tokens:
-            print(token)
+            print(f"TOKEN: {token}")
         return self.tokens
 
     def scan_token(self):
@@ -75,8 +75,13 @@ class Scanner:
             case '"':
                 self.string()
             case _:
-                pass
-                # error(self.line, "Unexpected character")
+                if self.is_digit(c):
+                    self.number()
+                elif self.is_alpha(c):
+                    self.identifier()
+                else:
+                    pass
+                    # error(self.line, "Unexpected character")
 
     def string(self) -> None:
         while self.peek() != '"' and not self.is_at_end():
@@ -89,6 +94,23 @@ class Scanner:
         self.advance()
         value: str = self.source[self.start + 1: self.current - 1]
         self.add_token_with_literal(TokenType.STRING, value)
+
+    def number(self) -> None:
+        while self.is_digit(self.peek()):
+            self.advance()
+        if self.peek() == '.' and self.is_digit(self.peek_next()):
+            self.advance()
+        while self.is_digit(self.peek()):
+            self.advance()
+        self.add_token_with_literal(TokenType.NUMBER, float(self.source[self.start: self.current]))
+
+    def identifier(self):
+        while self.is_alphanumeric(self.peek()):
+            self.advance()
+        text: str = self.source[self.start: self.current]
+        token_type: TokenType = keywords.get(text, TokenType.IDENTIFIER)
+        self.add_token(token_type)
+        
 
     def match(self, expected: chr) -> bool:
         if self.is_at_end():
@@ -104,6 +126,20 @@ class Scanner:
             return '\0'
 
         return self.source[self.current]
+
+    def peek_next(self) -> chr:
+        if self.current + 1 >= len(self.source):
+            return '\0'
+        return self.source[self.current + 1]
+
+    def is_digit(self, c: chr) -> bool:
+        return c >= '0' and c <= '9'
+
+    def is_alpha(self, c: chr) -> bool:
+        return (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or (c == '_')
+
+    def is_alphanumeric(self, c: chr) -> bool:
+        return self.is_digit(c) or self.is_alpha(c)
 
     def is_at_end(self) -> bool:
         return self.current >= len(self.source)
--- token_type.py
+++ token_type.py
@@ -49,4 +49,23 @@ class TokenType(Enum):
 
     EOF = 39
 
+keywords = {
+    "and": TokenType.AND,
+    "class": TokenType.CLASS,
+    "else": TokenType.ELSE,
+    "false": TokenType.FALSE,
+    "for": TokenType.FOR,
+    "fun": TokenType.FUN,
+    "if": TokenType.IF,
+    "nil": TokenType.NIL,
+    "or": TokenType.OR,
+    "print": TokenType.PRINT,
+    "return": TokenType.RETURN,
+    "super": TokenType.SUPER,
+    "this": TokenType.THIS,
+    "true": TokenType.TRUE,
+    "var": TokenType.VAR,
+    "while": TokenType.WHILE,
+}
+
 
